ad782797993e2d219716afd482e80e7db5c3654f
Anandhbasha <anadhakm@gmail.com>
2025-05-23 21:13:12 +0530

    router

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,63 @@
-// import Button from './Button'
-// function App(){
-//   const btnValues = ["Click","Login","Delete","Update","Submit"]
-//   const persons = [
-//     {personName:"xyz",
-//       age:30
-//     },{
-//       personName:"abc",
-//       age:40
-//     }
-//   ]
-//   return(
-//     <div className="App">
-//       {/* {btnValues.map((btn)=>(
-//         <button>{btn}</button>
-//       ))} */}
-//       {/* <button>Login</button>
-//       <button>Delete</button>
-//       <button>Update</button>
-//       <button>Submit</button> */}
-//       {btnValues.map((btnValues)=>(
-//         <Button values={btnValues} />
-//       ))}
-//       {persons.map((item)=>(
-//         <Button {...item} />
-//       ))}
+// // import Button from './Button'
+// // function App(){
+// //   const btnValues = ["Click","Login","Delete","Update","Submit"]
+// //   const persons = [
+// //     {personName:"xyz",
+// //       age:30
+// //     },{
+// //       personName:"abc",
+// //       age:40
+// //     }
+// //   ]
+// //   return(
+// //     <div className="App">
+// //       {/* {btnValues.map((btn)=>(
+// //         <button>{btn}</button>
+// //       ))} */}
+// //       {/* <button>Login</button>
+// //       <button>Delete</button>
+// //       <button>Update</button>
+// //       <button>Submit</button> */}
+// //       {btnValues.map((btnValues)=>(
+// //         <Button values={btnValues} />
+// //       ))}
+// //       {persons.map((item)=>(
+// //         <Button {...item} />
+// //       ))}
 
 
       
+// //     </div>
+// //   )
+// // }
+
+// // export default App
+
+
+
+// import React, { useEffect, useState } from 'react'
+
+// const App = () => {
+//   let [count,setCount] = useState(0)
+//   let [bg,setBg] = useState("dark")
+//   const handleAdd = ()=>{
+//     setCount(++count)
+//     console.log(count);
+    
+//   }
+//   const handlebg = ()=>{
+//     setBg(bg==="light"?"dark":"light")
+//   }
+
+
+//   useEffect(()=>{
+//     console.log("UseEffect");
+    
+//   },[count])
+//   return (
+//     <div className='App'>
+//       <button onClick={handleAdd}>{count}</button>
+//       <button onClick={handlebg} style={{backgroundColor:bg==="light"?"black":"white",color:bg==="light"?"white":"black"}}>{bg}</button>
 //     </div>
 //   )
 // }
@@ -35,48 +66,40 @@
 
 
 
-import React, { useEffect, useState } from 'react'
 
+
+
+
+
+
+
+
+
+
+
+//Router DOM
+import React from 'react'
+import Navbar from './Components/Navbar/Navbar'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import AllProduct from './Pages/AllProduct/AllProduct'
+import Mens from './Pages/Mens/Mens'
+import Womens from './Pages/Womens/Womens'
 const App = () => {
-  let [count,setCount] = useState(0)
-  let [bg,setBg] = useState("dark")
-  const handleAdd = ()=>{
-    setCount(++count)
-    console.log(count);
-    
-  }
-  const handlebg = ()=>{
-    setBg(bg==="light"?"dark":"light")
-  }
-
-
-  useEffect(()=>{
-    console.log("UseEffect");
-    
-  },[count])
   return (
-    <div className='App'>
-      <button onClick={handleAdd}>{count}</button>
-      <button onClick={handlebg} style={{backgroundColor:bg==="light"?"black":"white",color:bg==="light"?"white":"black"}}>{bg}</button>
-    </div>
+    <BrowserRouter>
+      <div className='App'>
+        <Navbar/>
+        <Routes>
+          <Route path='/' element={<AllProduct/>} />
+          <Route path='/mens' element={<Mens/>} />
+          <Route path='/womens' element={<Womens/>} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   )
 }
 
 export default App
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
